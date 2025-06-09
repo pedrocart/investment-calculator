ae2d0460eda90963c587f59146d7c49368a8c403
Pedro Costa <pedrocart@gmail.com>
2025-06-09 16:47:15 -0300

    Everything seems to be working but calculations are wrong.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+
+import Header from "./Header";
+import UserInput from "./UserInput";
+import { useState } from "react";
+
 function App() {
+    const [initialInvestment, setInitialInvestment] = useState("")
+    const [annualInvestment, setAnnualInvestment] = useState("")
+    const [expectedReturn, setExpectedReturn] = useState("")
+    const [duration, setDuration] = useState("")
+
+    const handleInitialInvestmentChange = (event) => {
+        setInitialInvestment(event.target.value)
+    }
+    const handleAnnualInvestmentChange = (event) => {
+        setAnnualInvestment(event.target.value)
+    }
+    const handleExpectedReturnChange = (event) => {
+        setExpectedReturn(event.target.value)
+    }
+    const handleDurationChange = (event) => {
+        setDuration(event.target.value)
+    }
+
   return (
-    <h1>React Investment Calculator</h1>
+    <main>
+      <Header />
+      <UserInput initialInvestment={initialInvestment} annualInvestment={annualInvestment} expectedReturn={expectedReturn} duration={duration} handleInitialInvestmentChange={handleInitialInvestmentChange} handleAnnualInvestmentChange={handleAnnualInvestmentChange} handleExpectedReturnChange={handleExpectedReturnChange} handleDurationChange={handleDurationChange}  />
+    </main>
   )
 }
 
